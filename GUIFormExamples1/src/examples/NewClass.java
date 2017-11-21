d43001028df2217af1cfb022e0548fb5b7401b7b
b1209wilson <bwilson@macon.k12.mo.us>
2017-11-21 14:02:18 -0600

    firstChange

---
+++ GUIFormExamples1/src/examples/NewClass.java
@@ -0,0 +1,15 @@
+package examples;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Macon Robotics
+ */
+public class NewClass {
+    
+}
